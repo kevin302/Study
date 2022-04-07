4fa9d99386f6adc18e7a569785fc133443a988f5
onasheep <inabass@naver.com>
2022-04-07 20:09:50 +0900

    HSJ

---
+++ HSJ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HSJ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
